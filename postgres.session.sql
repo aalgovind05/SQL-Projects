0f3e829d7d22750e788394c3242ba468796d3f72
aal_govind <aalgovind05@gmail.com>
2026-05-15 23:31:10 +0530

    customers discount total

--- postgres.session.sql
+++ postgres.session.sql
@@ -649,3 +649,14 @@ ORDER BY
   e.last_name,
   e.first_name,
   num_orders DESC
+
+--self made questions
+
+Select 
+YEAR(o.order_date) AS order_year, 
+ROUND(SUM(p.unit_price * od.quantity * od.discount):: NUMERIC,2) AS discount_amount
+from orders o 
+JOIN order_details od ON o.order_id = od.order_id
+JOIN products p ON od.product_id = p.product_id
+group by YEAR(o.order_date)
+order by order_year desc;
